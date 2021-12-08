4305fabed4dbd0cde9a5988e27d82618cdcdab09
thedon122 <donovan122@yahoo.com>
2021-12-08 15:34:00 +0100

    reducers update

---
+++ src/reducers/usersReducer.js
@@ -0,0 +1,8 @@
+export default (state = [], action) => {
+    switch (action.type) {
+        case 'Fetch_User':
+           return [...state, action.payload]; 
+        default:
+            return state; 
+    }
+};
